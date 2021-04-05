af4c950c7d1d68102a833af988eaec76c4c03000
Rony Maree <r.maree@flow4.com>
2021-04-05 19:07:31 +0200

    [section 2] - stuff

--- section-2/basics-01-starting-code/app.js
+++ section-2/basics-01-starting-code/app.js
@@ -1,1 +1,24 @@
-Vue.createApp();
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA: 'Helloo',
+            courseGoalB: 'Yoyoyo',
+            vueLink: 'https://www.google.com'
+        }
+    },
+
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
+    }
+
+});
+
+app.mount('#user-goal');
